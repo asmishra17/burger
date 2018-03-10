78c42606c14b713840e8cf0dda97cd709442775f
Aditi Mishra <asmishra93@gmail.com>
2018-03-10 17:23:45 -0600

    First attempt at model

--- models/burger.js
+++ models/burger.js
@@ -1,1 +1,21 @@
-var orm = require('./orm.js')
+// import the ORM to create functions that will interact with the database. 
+var orm = require('../config/orm.js')
+
+var burger = {
+    selectAll: function (cb) {
+        orm.selectAll('burgers', function(res) {
+            cb(res);
+        })
+    }, insertOne: function (colName1, colName2, value1, value2) {
+        orm.insertOne('burgers', colName1, colName2, value1, value2, function (res) {
+            cb(res);
+        })
+    }, updateOne: function (colName1, value1, colName2, value2) {
+        orm.updateOne('burgers', colName1, colName2, value1, value2, function (res) {
+            cb(res);
+        });
+    }
+};
+
+// export the database functions for the controller
+module.exports = burger;
