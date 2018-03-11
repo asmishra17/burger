a3508261b009b597616876cd8a7f80e335ed50ed
Aditi Mishra <asmishra93@gmail.com>
2018-03-10 18:28:29 -0600

    First attempt at router

---
+++ controllers/burgersController.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+// not entirely sure what express.Router() does
+
+// import the model (burger.js) to use its database functions
+var burger = require('../models/burger.js');
+
+// create all our routes and set up logic within those routes
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        var hbsObject = { // handlebars object?
+            burgers: data
+        }
+
+        console.log(hbsObject);
+        res.render('index', hbsObject);
+    });
+})
+
+// everything is displayed on index page?
+router.post("/", function (req, res) {
+    burger.insertOne([
+        'burger_name', 'devoured'
+    ], [
+        req.body.burger_name, req.body.devoured
+    ], function (result) {
+        // send back the ID of the new burger
+        res.json({id: result.insertId})
+    });
+}); // unsure what insertId is
+
+router.put("/", function (req, res) {
+    // insert logic here to change devoured from false to true probably...
+});
+
+module.exports = router;
