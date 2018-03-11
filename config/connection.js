3a7f53af9bc6c3a89c2f8d8f74ca8d95c9615bd8
Aditi Mishra <asmishra93@gmail.com>
2018-03-11 15:04:23 -0500

    Fixing connection and adding server.js

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,22 @@
-module.exports = mysql.creatConnection({
+var mysql = require('mysql');
+
+var connection = mysql.createConnection({
     host: 'localhost',
-    port: process.env.PORT || 3000,
+    port: 3306,
     // should be the same as server.js?
 
     user: 'root',
     password: '',
 
-    database: 'burgers'
+    database: 'burgers_db'
 });
 
+connection.connect(function(err) {
+    if (err) {
+        console.error(`Error connection: ${err.stack}`);
+        return;
+    }
+    console.log(`Connected as id ${connection.threadId}`);
+})
+
+module.exports = connection;
